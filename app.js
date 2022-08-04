68e73f786f2bfc003048df9debf141561f8b6623
Andrii Nezhyd <andrii_nezhyd@epam.com>
2022-08-04 20:47:19 +0300

    commit before 4nd deploy

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import express from "express"
 import mongoose from "mongoose"
 import router from "./routes/sale.routes.js";
 import cors from "cors";
+import path from "path"
 
 const app = express()
 const PORT = process.env.PORT || config.get("port") || 5000
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("/", (req, res) => {
   res.end("<div><ul><li><a href='/'>Home</a></li><li><a href='/about'>About</a></li></ul><h1>Home page</h1></div>")
@@ -29,7 +31,7 @@ async function start() {
       console.log(`Server has been started on port ${PORT}...`)
     })
   } catch (e) {
-    console.log("Server Error 23", e.message)
+    console.log("Server Error", e.message)
     process.exit(1)
   }
 }
